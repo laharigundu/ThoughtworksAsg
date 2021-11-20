9bd9b423c02bd5a5c5cc8557bb36ee84cdcbb4b9
laharigundu <50200291+laharigundu@users.noreply.github.com>
2021-11-20 20:05:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,37 +19,4 @@ public class Main {
             }
     }
 
-    /*  Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int z = sc.nextInt();
-        int n = sc.nextInt();
-        int[] a = new int[3];
-        int min = Integer.MAX_VALUE , Lab = -1;
-
-        a[0] = x - n;
-        a[1] = y - n;
-        a[2] = z - n;
-        for(int i = 0 ; i < 3 ; i++) {
-            if(min > a[i] && a[i] >= 0) {
-                min = a[i];
-                Lab = i;
-            }
-        }
-        if(x < 0 && y < 0 && z < 0) {
-            System.out.println("Not Possible");
-        }
-        else if(Lab == 0) {
-            System.out.println("L1");
-        }
-        else if(Lab == 1) {
-            System.out.println("L2");
-        }
-        else if(Lab == 2) {
-            System.out.println("L3");
-        }
-        else {
-            System.out.println("Not Possible");
-        }
-        */
 }
